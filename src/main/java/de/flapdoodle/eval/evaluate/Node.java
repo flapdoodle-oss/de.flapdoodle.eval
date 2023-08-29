42886542f35098eb8f1de75a382ae332e153d759
Michael Mosmann <michael@mosmann.de>
2023-08-29 16:36:35 +0200

    used vars

--- src/main/java/de/flapdoodle/eval/evaluate/Node.java
+++ src/main/java/de/flapdoodle/eval/evaluate/Node.java
@@ -6,10 +6,21 @@ import de.flapdoodle.eval.config.ValueResolver;
 import de.flapdoodle.eval.data.Value;
 import de.flapdoodle.eval.parser.Token;
 
+import java.util.AbstractSet;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public abstract class Node {
 	@org.immutables.value.Value.Parameter
 	protected abstract Token token();
 
 	@org.immutables.value.Value.Auxiliary
 	public abstract Value<?> evaluate(ValueResolver variableResolver, EvaluationContext context) throws EvaluationException;
+
+	public static Set<String> usedVariables(Node root) {
+		Set<String> ret = new LinkedHashSet<>();
+		
+		return Collections.unmodifiableSet(ret);
+	}
 }
