4f25fc1fec85175601a9903f4006d61402c7ae8e
Michael Mosmann <michael@mosmann.de>
2023-10-17 23:25:18 +0200

    core tests

---
+++ src/test/java/de/flapdoodle/eval/core/CoreTests.java
@@ -0,0 +1,11 @@
+package de.flapdoodle.eval.core;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Core Tests")
+@SelectPackages("de.flapdoodle.eval.core")
+public class CoreTests {
+}
