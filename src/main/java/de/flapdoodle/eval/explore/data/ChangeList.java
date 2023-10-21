a326a7d8277f593c93a8583578a9605602739603
Michael Mosmann <michael@mosmann.de>
2023-10-21 09:17:28 +0200

    cleanup

--- src/main/java/de/flapdoodle/eval/explore/data/ChangeList.java
+++
@@ -1,7 +0,0 @@
-package de.flapdoodle.eval.explore.data;
-
-import java.util.List;
-
-public interface ChangeList<T> {
-	List<Change<T>> changes();
-}
--- src/main/java/de/flapdoodle/eval/explore/model/Processor.java
+++
@@ -1,8 +0,0 @@
-package de.flapdoodle.eval.explore.model;
-
-import java.time.Duration;
-import java.time.LocalDate;
-
-public interface Processor<S, D> {
-	Transaction<S, D> process(LocalDate date, S source, D destination, Duration duration);
-}
